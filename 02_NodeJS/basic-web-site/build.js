84557a3d85624286008e8710bbe6419db5f1b47d
oricalvo <ori.calvo@gmail.com>
2018-03-25 16:17:57 +0300

    more

--- 02_NodeJS/basic-web-site/build.js
+++ 02_NodeJS/basic-web-site/build.js
@@ -1,3 +1,30 @@
+const child_process = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const opn = require("opn");
+
 const command = process.argv[2];
 
-console.log(command);
+if(command == "dev") {
+    dev();
+}
+else {
+    console.error("Unknown command: " + command);
+}
+
+function dev() {
+    fs.readFile(path.resolve(__dirname, "config.json"), "utf8", function(err, data) {
+        const config = JSON.parse(data);
+
+        child_process.spawn(path.resolve("node_modules/.bin/http-server"), [
+            "-p",
+            config.port,
+        ], {
+            shell: true,
+            stdio: "inherit",
+        });
+
+        opn(`http://localhost:${config.port}`);
+    });
+}
+
